68ae79eeb96cc3a495bc3204837d61a36a421fa9
usamimi39 <usamin04tp@gmail.com>
2026-08-18 12:22:06 +0900

    ogp

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,12 @@ import { defineConfig, fontProviders } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 
 import rehypeEmbed from './src/plugins/rehype-embed/index.mjs';
+import ogImages from './src/integrations/og-images.mjs';
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), ogImages()],
 
   // 本文中の「URL だけの段落」を YouTube / Spotify / Twitter / Misskey の
   // 埋め込みに自動変換する。MDX も markdown 設定を継承するため両方に適用される。
